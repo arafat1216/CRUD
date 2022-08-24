8a6666cc503e78cc5eb31c7302205a22de40466a
Arafat Hossain <arafathossain0895@gmail.com>
2022-08-24 12:55:38 +0600

    Add project files.

---
+++ CRUD/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult AllCourses()
+        {
+            return View();
+        }
+    }
+}
